f2c2188d3206fa9394f4432da923a09f1f9792e6
Medhat Mohamed <35978271+MedhatM7tt@users.noreply.github.com>
2018-02-13 23:29:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/heba/testproject/DocLoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.heba.testproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DocLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_login);
+    }
+    public void doctorLogin(View view)
+    {
+        ((Button) view).setBackgroundResource(R.drawable.clicked_btn);
+    }
+}
---
+++ app/src/main/java/com/example/heba/testproject/StudentLoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.heba.testproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+    public void studentLogin(View view)
+    {
+        ((Button) view).setBackgroundResource(R.drawable.clicked_btn);
+
+    }
+    public void reg(View view)
+    {
+        startActivity(new Intent(this,RegActivity.class));
+    }
+}
